c9e2122db2fbdd54f8863e575917e275172a18ab
jiaming.lu@philips.com <jiaming.lu@philips.com>
2024-06-07 08:50:38 +0800

    ScriptManager&Random

---
+++ PD.LL.Common/RandomUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PD.LL.Common
+{
+    public class RandomUtil
+    {
+
+        /// <summary>
+        /// 随机生成指定长度的字符串
+        /// </summary>
+        /// <param name="pwdLength">字符长度</param>
+        /// <returns></returns>
+        public static string CreatePassword(int pwdLength)
+        {
+            string resultStr = "";
+            string pwdchars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int iRandNum;
+            Random rnd = new Random();
+            for (int i = 0; i < pwdLength; i++)
+            {
+                iRandNum = rnd.Next(pwdchars.Length);
+                resultStr += pwdchars[iRandNum];
+            }
+
+            return resultStr;
+
+        }
+    }
+}
---
+++ PD.LL.Common/ScriptManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace PD.LL.Common
+{
+    public class ScriptManager
+    {
+        /// <summary>
+        /// 动态执行脚本
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        /// <exception cref="CompilationErrorException"></exception>
+        /// <exception cref="Exception"></exception>
+        public static async Task<object> ExecuteScript(string expression)
+        {
+            
+            //sample
+            //string expression = @"  if (3 >2) {return 1 ;} else {return  0;}";//"2 + 3 * 4"; // 要执行的表达式
+            // 创建脚本运行时
+            var scriptOptions = ScriptOptions.Default;
+            var script = CSharpScript.Create(expression, options: scriptOptions);
+        
+            try
+            {
+                // 执行脚本
+                var result = await script.RunAsync();
+
+                return result.ReturnValue;
+            }
+            catch (CompilationErrorException ex)
+            {
+                // 如果代码编译错误，则捕获并输出错误信息
+                Console.WriteLine("Compilation Error: " + string.Join(Environment.NewLine, ex.Diagnostics));
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                // 其他异常情况
+                Console.WriteLine("Error: " + ex.Message);
+                throw ex;
+            }
+        }
+
+      
+
+    }
+}
